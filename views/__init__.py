289634e599e38bf7b0354a6f057392b2f3033fc7
Aleksandr Beliakov <genzksync0@gmail.com>
2025-11-28 11:01:39 +0300

    add new view - listener new_messages from channel

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 
+from views.new_refernce_view.new_ref_view import new_ref_router
 from views.search_view.search_view import search_router
 from views.start_view.start_view import start_router
 from views.suggestion_view.suggestion_view import suggestion_router
@@ -7,7 +8,7 @@ from views.suggestion_view.suggestion_view import suggestion_router
 
 router = Router()
 
-
+router.include_router(new_ref_router)
 router.include_router(start_router)
 router.include_router(suggestion_router)
 router.include_router(search_router)
