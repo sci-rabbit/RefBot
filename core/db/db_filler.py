5d92165a77335c5514294a456743a9b139e952e7
Aleksandr Beliakov <genzksync0@gmail.com>
2025-11-28 12:48:14 +0300

    use async contextmanager in db_filler

--- core/db/db_filler.py
+++ core/db/db_filler.py
@@ -1,8 +1,8 @@
 import structlog
 from sqlalchemy.exc import IntegrityError, DataError, StatementError
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from config import settings
+from core.db.database import get_session
 from core.media_utils import download_media
 from core.models.messages import Message
 from telethon import TelegramClient
@@ -11,7 +11,6 @@ logger = structlog.getLogger(__name__)
 
 
 async def db_filler(
-    session: AsyncSession,
     client: TelegramClient,
     source_chat: int = settings.bot.source_chat,
 ) -> None:
@@ -23,36 +22,38 @@ async def db_filler(
         "🔍 Начало поиска: chat=%r",
         source_chat,
     )
-    async for msg in client.iter_messages(
-        source_chat,
-    ):
-        logger.debug(
-            "Найдено сообщение message_id=%s",
-            msg.id,
-        )
-        if msg.media:
-            try:
-                msg_photo_bytes = await download_media(msg)
-                message = Message(
-                    message_id=msg.id,
-                    media_group_id=msg.grouped_id,
-                    hash_tags=msg.message,
-                    photo=msg_photo_bytes,
-                )
-                session.add(message)
-                await session.commit()
-            except (
-                DataError,
-                StatementError,
-                IntegrityError,
-            ) as e:
-                logger.exception(
-                    "Ошибка базы данных во время добавления элементов",
-                    error=str(e),
-                    exc_info=True,
-                )
-                await session.rollback()
-                continue
+
+    async with get_session() as session:
+        async for msg in client.iter_messages(
+            source_chat,
+        ):
+            logger.debug(
+                "Найдено сообщение message_id=%s",
+                msg.id,
+            )
+            if msg.media:
+                try:
+                    msg_photo_bytes = await download_media(msg)
+                    message = Message(
+                        message_id=msg.id,
+                        media_group_id=msg.grouped_id,
+                        hash_tags=msg.message,
+                        photo=msg_photo_bytes,
+                    )
+                    session.add(message)
+                    await session.commit()
+                except (
+                    DataError,
+                    StatementError,
+                    IntegrityError,
+                ) as e:
+                    logger.exception(
+                        "Ошибка базы данных во время добавления элементов",
+                        error=str(e),
+                        exc_info=True,
+                    )
+                    await session.rollback()
+                    continue
 
     logger.info("✅ Поиск завершён")
     logger.info(
