bf2b07f45243bc4603deebf32dd4eb312deb92ab
Aleksandr Beliakov <genzksync0@gmail.com>
2025-11-28 12:49:15 +0300

    upd middleware add check redis client

--- middleware.py
+++ middleware.py
@@ -1,8 +1,11 @@
+import structlog
 from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery
 
 from config import settings
 from redis_client.redis import AsyncRedisClient
+
+logger = structlog.getLogger(__name__)
 
 
 class RateLimitMiddleware(BaseMiddleware):
@@ -16,25 +19,34 @@ class RateLimitMiddleware(BaseMiddleware):
         super().__init__()
 
     async def __call__(self, handler, event, data):
-        redis = await AsyncRedisClient.get_client()
+        redis = None
+        try:
+            redis = await AsyncRedisClient.get_client()
+        except Exception as e:
+            logger.error("Redis недоступен", error=str(e), exc_info=True)
+
         user_id = getattr(event, "from_user", None)
         if user_id is None:
             return await handler(event, data)
 
-        key = f"rate:{user_id.id}"
-        current = await redis.get(key)
-        current = int(current) if current else 0
+        if redis:
+            key = f"rate:{user_id.id}"
+            current = await redis.get(key)
+            current = int(current) if current else 0
 
-        if current >= self.limit:
-            if isinstance(event, Message):
-                await event.answer("⏳ Слишком часто, подожди!")
-            elif isinstance(event, CallbackQuery):
-                await event.answer("⏳ Слишком часто, подожди!", show_alert=True)
-            return
+            if current >= self.limit:
+                if isinstance(event, Message):
+                    await event.answer("⏳ Слишком часто, подожди!")
+                elif isinstance(event, CallbackQuery):
+                    await event.answer("⏳ Слишком часто, подожди!", show_alert=True)
+                return
 
-        async with redis.pipeline(transaction=True) as pipe:
-            await pipe.incr(key)
-            await pipe.expire(key, self.period)
-            await pipe.execute()
+            async with redis.pipeline(transaction=True) as pipe:
+                await pipe.incr(key)
+                await pipe.expire(key, self.period)
+                await pipe.execute()
 
         return await handler(event, data)
+
+
+rate_middleware = RateLimitMiddleware()
